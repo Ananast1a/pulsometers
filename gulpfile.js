40f34df17866a6c7dbbe0ba39c6d18bbf2e42923
Anastasia Ly <lytvynovanasty@gmail.com>
2021-03-11 17:40:14 +0200

    container added

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,9 @@ gulp.task('server', function() {
     browserSync({
         server: {
             baseDir: "src"
-        }
+        },
+        browser: "google chrome"
     });
-
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
